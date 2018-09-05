d3aa4c8c362e9f79208a7b5d47684a79ff964a3e
Andhitia Rama <andhitia.r@gmail.com>
2018-09-05 07:30:19 +0700

    [8.0.1.0.0] hr_service_recruitment

---
+++ pfi_hr_service_recruitment/models/recruitment_candidate.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class RecruitmentCandidate(models.Model):
+    _inherit = ["hr_service.recruitment_candidate"]
+
+    objective = fields.Text(
+        string="Objective",
+    )
+    cover_letter = fields.Text(
+        string="Cover Letter",
+    )
+    last_work = fields.Text(
+        string="Last Work",
+    )
+    last_education = fields.Text(
+        string="Last Education",
+    )
