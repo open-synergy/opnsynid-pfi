427ebcada20b0aee77f5c156552770b20bbe4f22
Andhitia Rama <andhitia.r@gmail.com>
2018-09-05 07:30:19 +0700

    [8.0.1.0.0] hr_service_recruitment

---
+++ pfi_hr_service_recruitment/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "PFI Extension for HR Service Recruitment",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "license": "AGPL-3",
+    "depends": [
+        "hr_service_recruitment",
+    ],
+    "data": [
+        "views/recruitment_candidate_views.xml",
+    ],
+    "installable": True
+}
